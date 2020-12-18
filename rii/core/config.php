80eab54711d9b1e44fa7d0e5d86130d8ab274a15
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-18 13:11:03 +0300

    Adding class Config

---
+++ rii/core/config.php
@@ -0,0 +1,39 @@
+<?php namespace Rii\Core\Config;
+
+    class Config
+    {
+        private static $data = [];
+
+        protected function __construct() { }
+
+        private static function init()
+        {
+            if (empty(self::$data)) {
+                self::$data = include_once ($_SERVER['DOCUMENT_ROOT']."/rii"."/config.php");
+            }
+        }
+
+        public static function get(string $requestData)
+        {
+            self::init();
+
+            $requestKey = explode("/", $requestData);
+
+            $searchData = self::$data;
+
+            for ($i = 0; $i < count($requestKey); $i++) {
+                if (array_key_exists($requestKey[$i], $searchData)) {
+                    if (is_array($searchData[$requestKey[$i]])){
+                        $result = $searchData = $searchData[$requestKey[$i]];                     
+                    } else{
+                        $result = $searchData[$requestKey[$i]];
+                        break;
+                    }
+                }
+            }
+
+            return $result;
+        }
+    }
+
+?>
