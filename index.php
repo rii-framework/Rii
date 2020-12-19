ef492398277bc6f5ba6f13f5e494656cdf6f0257
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-19 17:03:08 +0300

    Test

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <!--Дневник проекта-->
+<?php
+require_once "rii/core/page.php";
+
+use Rii\Core\Page\Page as Page;
+
+Page::getInstance()->setProperty("title", "Прикол");
+
+echo Page::getInstance()->getProperty("title");
+
+<pre>
+    19.12.20
+    - Исправил класс Config
+    - Создал класс Page
+        - Создание полей
+        - Создание метовод
+            - setPropery
+            - getPropery
+            - showPropery
+</pre>
+
+?>
