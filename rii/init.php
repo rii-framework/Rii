facdcf902266a8866c088199d94dcbcfeb5eafcc
Роман <dudoitsrv@gmail.com>
2020-12-19 13:36:00 +0300

    function autoload

--- rii/init.php
+++ rii/init.php
@@ -3,15 +3,19 @@ session_start();
 
 spl_autoload_register(function($class) {
 
-    $className = '../' .$class  . '.php';
+    $className = mb_strtolower($class);
+    $className = '../' .$className  . '.php';
     $className = str_replace('\\', '/', $className);
 
     if (file_exists($className)) require $className;
 });
 
+$test = new Rii\Core\test();
+
 //$test = new Rii\Core\test();
 // функция получает имя класса типа Rii\Core\
-//// т.к. по условию задачи все namespace должны начинаться Rii\Директория\
+// переводим имя класса в нижний регистр
+// т.к. по условию задачи все namespace должны начинаться Rii\Директория\
 // в переменную className формируем путь к файлу относить файла init.php
 // путь начинается с '../' нужно выйти из текущей директории, а потом заходим по основному
 // пути который передался в $class + расширение .php
