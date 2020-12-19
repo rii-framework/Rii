64fee5b24a8a0c8db10da912743caf8ba8eec9ff
Роман <dudoitsrv@gmail.com>
2020-12-19 16:46:39 +0300

    function autoload

--- rii/init.php
+++ rii/init.php
@@ -1,13 +1,19 @@
 <?php
 session_start();
 
+define('RII_CORE_INCLUDED', true);
+
 spl_autoload_register(function($class) {
 
     $className = mb_strtolower($class);
     $className = '../' .$className  . '.php';
     $className = str_replace('\\', '/', $className);
 
-    if (file_exists($className)) require $className;
+    if (file_exists($className))
+    {
+
+        require $className;
+    }
 });
 
 $test = new Rii\Core\test();
@@ -21,3 +27,5 @@ $test = new Rii\Core\test();
 // пути который передался в $class + расширение .php
 // после заменим обратный слеш
 // проверяем если такой файл, если есть подключаем
+//константа RII_CORE_INCLUDED предназначена для проверки подключено ли ядро
+//
