3e15805e451edf00e43c87885ad8246d2d20e3b8
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-19 16:57:38 +0300

    Creating class Page

--- rii/core/page.php
+++ rii/core/page.php
@@ -1,8 +1,67 @@
-<?php
+<?php namespace Rii\Core\Page;
 
-namespace Rii\Core\Config;
+    class Page
+    {
+        private static $instances = [];
+        private $property = [];
+        private $addList = [];
+        private $macros = [];
+        
+        //Скрытие конструктора
+        protected function __construct() { }
+        
+        //Скрытие клонирования
+        protected function __clone() { }
+        
+        //Скрытие востановления  
+        public function __wakeup() { }
 
-class Page
-{
+        static function getInstance(): Page
+        {
+            $cls = static::class;
 
-}
+            if (!isset(self::$instances[$cls])) {
+                self::$instances[$cls] = new static();
+            }
+
+            return self::$instances[$cls];
+        }
+
+        //Получение свойства по ключу
+        public function getProperty(string $id): string
+        {
+            return $this->$property[$id];
+        }
+
+        //Добавление свойства для хранение по ключу
+        public function setProperty($id, $value): void
+        {
+            $this->$property[$id] = $value;
+        }
+
+        //Выводит макрос для будущей замены #RII_PAGE_PROPERY_{$id}#
+        public function showProperty($id): string
+        {
+            return "#RII_PAGE_PROPERY_{$id}#";
+        }
+        
+        //Добавляет src в массив сохраняя уникальность
+        static function addJs(string $path): void
+        {
+            $this->$addList[$path] = true;
+        }
+
+        //Добавляет link сохраняя уникальность
+        static function addCss(string $path): void
+        {
+            $this->$addList[$path] = true;
+        }
+
+        //Добавляет в массив для хранения
+        /*static function addString(string $path): void
+        {
+            
+        }*/
+    }
+
+?>
