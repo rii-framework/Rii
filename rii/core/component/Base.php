55cc53fb2c614b4007cfc555908a0fd9febf7b54
Роман <dudoitsrv@gmail.com>
2021-01-05 12:21:49 +0300

    corrected construct

--- rii/core/component/Base.php
+++ rii/core/component/Base.php
@@ -5,11 +5,11 @@ namespace Rii\Core\component;
 abstract class Base
 {
 
-    public $result;
-    public $id;
-    public $params;
-    public $template;
-    public $_path;
+    public $result; // результат работы компонента
+    public $id; // имя компонента
+    public $params; // параметры для инициализации компонента
+    public $template; // экземпляр компонента
+    public $_path; // путь к компоненту
 
     public function __construct($id, $params, $template, $_path)
     {
