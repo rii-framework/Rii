3948f7ebe165723a63b4bf2e1dacd4f3ab369546
Роман <dudoitsrv@gmail.com>
2021-01-05 13:54:31 +0300

    corrected class

--- rii/core/component/Base.php
+++ rii/core/component/Base.php
@@ -5,18 +5,21 @@ namespace Rii\Core\component;
 abstract class Base
 {
 
-    public $result; // результат работы компонента
-    public $id; // имя компонента
-    public $params; // параметры для инициализации компонента
-    public $template; // экземпляр компонента
+    public $result = []; // результат работы компонента
+    public $id = 'element.list'; // имя компонента
+    public $params = []; // параметры для инициализации компонента
+    public $template = 'default'; // экземпляр компонента
     public $_path; // путь к компоненту
 
-    public function __construct($id, $params, $template, $_path)
+    public function __construct($id, $template = null, $params = [])
     {
-        $this->id = $id;
-        $this->params = $params;
-        $this->template = $template;
-        $this->_path = $_path;
+        if ($template != null)
+        {
+            $this->id = $id;
+            $this->params = $params;
+            $this->template = $template;
+            $this->_path = $_SERVER['DOCUMENT_ROOT'] . '/' . $id;
+        }
     }
 
     abstract public function executeComponent(); // метод обязателен для переопределения
