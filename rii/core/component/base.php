4be50328e0012cd864d498217dbbda4670334858
kokocikys <chernecky_ilia@rambler.ru>
2021-01-06 13:14:45 +0300

    Application ready

---
+++ rii/core/component/base.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Rii\Core\Component;
+
+abstract class Base
+{
+    public $result = []; // результат работы компонента
+    public $id = ''; // имя компонента
+    public $params = []; // параметры для инициализации компонента
+    public $template = null; // экземпляр компонента
+    public $__path; // путь к компоненту
+
+    public function __construct($id, $template = null, $params = [])
+    {
+        $this->id = $id;
+        $this->params = $params;
+        $this->__path = $_SERVER['DOCUMENT_ROOT'] . "/rii/components/" . $id . "/"; // Rii/rii/components/rii/component_id
+        $this->__path = str_replace(':', '/', $this->__path);
+        if ($template != null)
+        {
+            if ($template == '')
+            {
+                $this->template = 'default';
+            }
+            $this->template = new Template($template, $this);
+        }
+    }
+
+    abstract public function executeComponent(); // метод обязателен для переопределения
+}
