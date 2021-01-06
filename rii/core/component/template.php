5a59c6c26a69c0a05537f01db9df8506c5c46a47
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2021-01-06 12:46:29 +0300

    Refactoring Template class

--- rii/core/component/template.php
+++ rii/core/component/template.php
@@ -2,49 +2,55 @@
 
 namespace Rii\Core\Сomponent;
 
+use Rii\Core\Page;
+
 class Template
 {
-    private static $__path; //путь к шаблону на сервере
-    private static $__relativePath; //url к папке с шаблоном
-    private static $namespace = "rii"; // пространство имён, rii если внутренний компонент
-    private static $id = "ElementList"; // имя компонент
-    private static $def = "default"; // название папки с шаблоном по умолчанию
+    public $__path; //Путь к шаблону компонента tepmlates
+    public $__relativePath; //URL к папке с шаблоном
+    public $id; //Имя шаблона
+    public $component; //Компонент
+    private $page = null;
 
-    public function __construct($component)
+    public function __construct($id, $component)
     {
-        self::$namespace = $component::$namespace;
-        self::$id = $component::$id;
+        if (!file_exists($component->__path . "tepmlates/" . $id)) {
+            throw new Exception("Папка с шаблоном не найдена!");
+        }
 
-        if ($component::$def !== "") {
-            self::$def = $component::$def;
-        }            
+        $this->id = $id;
+        $this->component = $component;
+
+        $this->__relativePath = pathinfo($_SERVER["REQUEST_URI"])["dirname"] . "/tepmlates/" . $this->id . "/";
+        $this->__path =  $this->component->__path . "tepmlates/" . $this->id . "/";
+
+        $this->page = Page::getInstance();
     }
 
     //Подключение файла шаблона | $page - страница подключения в шаблоне. По дефолту template.php 
-    public static function render($page = "template"): void
+    public function render($page = "template"): void
     {
-        self::$__path = "/components/" . self::$namespace . "/" . self::$id . "/templates/" . self::$def; // /components/rii/ElementList/templates/default
+        $result = $this->component->result;
+        $params = $this->component->params;
 
-        self::$__relativePath = $_SERVER['DOCUMENT_ROOT'] . "/rii" . self::$__path; // /Rii/rii/components/rii/ElementList/templates/default
-
-        if (file_exists(self::$__relativePath . "/result_modifier.php")) {
-            require_once (self::$__relativePath . "/result_modifier.php");
+        if (file_exists($this->__path . "/result_modifier.php")) {
+            include $this->__path . "/result_modifier.php";
         }
 
-        if (file_exists(self::$__relativePath . "/" . $page . ".php")) {
-            require_once (self::$__relativePath . "/" . $page . ".php");
+        if (file_exists($this->__path . "/" . $page . ".php")) {
+            include ($this->__path . "/" . $page . ".php");
         }
 
-        if (file_exists(self::$__relativePath . "/component_epilog.php")) {
-            require_once (self::$__relativePath . "/component_epilog.php");
+        if (file_exists($this->__path . "/component_epilog.php")) {
+            include ($this->__path . "/component_epilog.php");
         }
 
-        if (file_exists(self::$__relativePath . "/style.css")) {
-            //Page::addCss(self::$__relativePath . "/style.css");
+        if (file_exists($this->__path . "/style.css")) {
+            $this->page->addCss($this->__relativePath . "/style.css");
         }
 
-        if (file_exists(self::$__relativePath . "/script.js")) {
-            //Page::addJs(self::$__relativePath . "/script.js");
+        if (file_exists($this->__path . "/script.js")) {
+            $this->page->addJs($this->__relativePath . "/script.js");
         }
     }    
 }
