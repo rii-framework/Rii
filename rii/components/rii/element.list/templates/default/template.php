258ce746433991b3702d82f468a0f1a5b7ccfc7d
kokocikys <chernecky_ilia@rambler.ru>
2021-01-12 17:56:53 +0300

    history.json/template.php update

--- rii/components/rii/element.list/templates/default/template.php
+++ rii/components/rii/element.list/templates/default/template.php
@@ -1,27 +1,24 @@
 <?php if (!defined('RII_CORE_INCLUDED')) die;
 
-//Здесь можно будет сделать json_encode при добавлении новых записей через форму отправки
-
 $strJsonFileContent = file_get_contents($_SERVER['DOCUMENT_ROOT'] . $params["data_file"]);   //Получаем содержимое upload/history.json в виде строки
 $array = json_decode($strJsonFileContent, true);   //Преобразуем строку в массив
-$reversedArray = array_reverse($array["listOfChanges"]);
+$reversedArray = array_reverse($array);
 
 $page = $_GET['page'];
 $limit = $params["limit"];
 $offset = $limit * ($page - 1);
 
+$content = "";
 for ($i = $offset; $i < $offset + $limit; $i++) {
     if ($reversedArray[$i]) {
-        echo "<div class='post'>Дата: " . $reversedArray[$i]["date"] . "<br>Разработчик: " . $reversedArray[$i]["programmer"] . "<br>Изменения:<br>";
-        foreach ($reversedArray[$i]["changes"] as $item) {
-            echo "<div class ='changes'>" . $item . "</div>";
-        }
-        echo "</div>";
+        $content .= "<div class='post'>Дата: " . $reversedArray[$i]["date"] . "<br>Разработчик: " . $reversedArray[$i]["programmer"] . "<br>Изменения:<br>";
+        $content .= "<div class ='changes'>" . $reversedArray[$i]["changes"] . "</div>";
+        $content .= "</div>";
     }
 }
 
-$pagLink = "<div class='pagination'>";
+$content .= "<div class='pagination'>";
 for ($i = 1; $i <= ceil(count($reversedArray) / $limit); $i++) {
-    $pagLink .= "<a href='index.php?page=" . $i . "'>" . $i . "</a>";
+    $content .= "<a href='index.php?page=" . $i . "'>" . $i . "</a>";
 }
-echo $pagLink . "</div>";
+echo $content .="</div>";
