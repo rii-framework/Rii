b1f8f03b69c07be6c27e3af87c6096c978dfd818
kokocikys <chernecky_ilia@rambler.ru>
2021-01-13 11:37:57 +0300

    variant 2

--- rii/components/rii/element.list/templates/default/template.php
+++ rii/components/rii/element.list/templates/default/template.php
@@ -1,3 +1,13 @@
 <?php if (!defined('RII_CORE_INCLUDED')) die;
 
-echo $result["element.list"];
+for ($i = $result["paginationParams"]["offset"]; $i < $result["paginationParams"]["offset"] + $result["paginationParams"]["limit"]; $i++) {
+    if ($result["data"][$i]) {
+        echo "<div class='post'>Дата: " . $result["data"][$i]["date"] . "<br>Разработчик: " . $result["data"][$i]["programmer"] . "<br>Изменения:<br><div class ='changes'>" . $result["data"][$i]["changes"] . "</div></div>";
+    }
+}
+
+echo "<div class='pagination'>";
+for ($i = 1; $i <= ceil(count($result["data"]) / $result["paginationParams"]["limit"]); $i++) {
+    echo "<a href='index.php?page=" . $i . "'>" . $i . "</a>";
+}
+echo "</div>";
