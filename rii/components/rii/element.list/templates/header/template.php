67acb7543bf097c3b1a58ac95b6ec6f49408e824
Роман <dudoitsrv@gmail.com>
2021-01-20 17:21:18 +0300

    add css file and add stile header and footer

--- rii/components/rii/element.list/templates/header/template.php
+++ rii/components/rii/element.list/templates/header/template.php
@@ -1,11 +1,13 @@
 <?php if (!defined('RII_CORE_INCLUDED')) die; ?>
 
 
-<div class="header--menuList">
+<section class="s-section section-black section--index" data-section="index-section">
+    <div class="header--menuList">
 <?php for ($i = 0; $i <= count($result); $i++) {
    echo "<a href=\"" .  $result["data"][$i]["link"] . "\""
        . "class=\"item js-click-section\""
        . "data-click=\"" . $result["data"][$i]["params"]["data-click"] . "\"" . ">"
        . $result["data"][$i]["title"] . "</a>";
 } ?>
-</div>
+    </div>
+</section>
--- rii/templates/default/header.php
+++ rii/templates/default/header.php
@@ -8,9 +8,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <? \Rii\Core\Page::getInstance()->addCss(self::getInstance()->getTemplatePath() . '/style/bootstrap.css'); ?>
-     <? self::getInstance()->includeComponent("rii:element.list", "header", ['data_type' => 'json', 'data_file' => '/rii/db/menu.json']); ?>
+    <? \Rii\Core\Page::getInstance()->addCss(self::getInstance()->getTemplatePath() . '/style/main.css'); ?>
+    <? \Rii\Core\Page::getInstance()->addCss(self::getInstance()->getTemplatePath() . '/style/fonts.css'); ?>
     <title><? \Rii\Core\Page::getInstance()->showProperty('Title'); ?></title>
     <? \Rii\Core\Page::getInstance()->showHead(); ?>
 </head>
 <body>
-<h2>Дневник проекта</h2>
+<? self::getInstance()->includeComponent("rii:element.list", "header", ['data_type' => 'json', 'data_file' => '/rii/db/menu.json']); ?>
+
+
+
+
+
+
+
+
+
+
