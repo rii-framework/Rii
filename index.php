107d3434cb70a8156ecdc29ca444f010bb6de0dc
Nineseven <nine@nineseven.ru>
2021-01-21 09:22:24 +0300

    test

--- index.php
+++ index.php
@@ -99,6 +99,7 @@ Page::getInstance()->setProperty('Title', "История изменений");
 //?>
 <!---->
 <!--<h2>История изменений проекта:</h2>-->
+
 <?php
 //$app->includeComponent("rii:element.list", "default", ['sort' => ['date' => 'desc'], 'limit' => 5, 'data_type' => 'json', 'data_file' => '/upload/history.json']);
 //?>
