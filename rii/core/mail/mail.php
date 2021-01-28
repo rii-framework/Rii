29d3d54644775233e64b0b8fbd01e09ad9819f9f
RamanDudoits <didoitsrv@gmail.com>
2021-01-28 17:59:53 +0300

    corrected class mail.php

--- rii/core/mail/mail.php
+++ rii/core/mail/mail.php
@@ -4,61 +4,123 @@ namespace Rii\Core\Mail;
 class Mail
 {
     private $template;
-    private $headers;
     private $params = [];
-    private $result = [];
+    private $settings = [];
+    private $headers;
+    private $message;
+    private $files;
 
-    public function __construct($params, $headers = "")
+
+    public function __construct($params)
     {
         $this->params = $params;
         $this->template = new Template($params["template"], $params["field"]);
-        $this->result = $this->template->getSettings();
-        $this->headers = $headers;
-        $this->setHeaders();
+        $this->settings = $this->template->getSettings();
+        $this->files = $this->template->getAttachFile();
+        $this->message = $this->template->getTemplate();
+        $this->headers = $this->setHeaders();
     }
 
     public function send()
     {
-        $to = $this->result["to"];
-        $file = $this->template->getAttachFile();
-        $message = $this->template->getTemplate();
+        return mail($this->settings["to"], $this->settings["subject"], $this->message, $this->headers);
+    }
 
-        if (!empty($this->params["file"]) || !empty($file))
+    private function setHeadersFrom()
+    {
+        $form = "";
+        if (empty($this->params["headers"]["from"]))
         {
-            $this->headers .= $this->sendFiles($this->checkFiles($file));
+            $form = "From: " . $this->params["headers"]["from"]  . "\r\n";
+        } elseif (!empty($this->result['from']))
+        {
+            $form = "From: " . $this->settings['from']  . "\r\n";
         }
-       return mail($to, $this->result["subject"], $message, $this->getHeaders());
+        return $form;
     }
 
-    private function setHeaders()
+    private function setHeadersReplyTo()
     {
-        $this->headers .= "From: " . $this->result['from']  . "\r\n";
-        $this->headers .= "Reply-To: " . $this->result['from'] . "\r\n";
-        $this->headers .= "Cc: " . $this->result['fromCopy'] . "\nBcc: " . $this->result['sett']['fromCopyH'] . "\n";
-        $this->headers .= "Content-type: text/html; charset=utf-8\n";
-        $this->headers .= "X-Mailer: PHP/" . phpversion();
+        $replyTo = "";
+        if (empty($this->params["headers"]["reply-to"]))
+        {
+            $replyTo .= "Reply-To: " . $this->params["headers"]["reply-to"]  . "\r\n";
+        } elseif (!empty($this->settings['from']))
+        {
+            $replyTo .= "Reply-To: " . $this->settings['from'] . "\r\n";
+        }
+        return $replyTo;
     }
 
-    public function getHeaders()
+    private function setHeadersCc()
     {
+        $cc = "";
+        if (empty($this->params["headers"]["cc"]))
+        {
+            $cc .= "Cc: " . $this->params["headers"]["cc"]  . "\n";
+        } elseif (!empty($this->settings['fromCopy']))
+        {
+            $cc .= "Cc: " . $this->settings['fromCopy'] . "\n";
+        }
+        return $cc;
+    }
+
+    private function setHeadersBcc()
+    {
+        $bcc = "";
+        if (empty($this->params["headers"]["Bcc"]))
+        {
+            $bcc .= "Bcc: " . $this->params["headers"]["Bcc"]  . "\n";
+        } elseif (!empty($this->settings['fromCopyH']))
+        {
+            $bcc .= "Bcc: " . $this->settings['fromCopyH'] . "\n";
+        }
+        return $bcc;
+    }
+
+    private function setHeadersContentType()
+    {
+        $contentType = "";
+        if (empty($this->params["headers"]["content-type"]))
+        {
+            $contentType .= "Content-type: " . $this->params["headers"]["content-type"]  . "\n";
+        } else $contentType .= "Content-type: text/html; charset=utf-8\n";
+        $contentType .= "X-Mailer: PHP/" . phpversion();
+
+        return $contentType;
+
+    }
+
+    public function setHeaders()
+    {
+        $this->headers = $this->setHeadersFrom();
+        if (empty($this->headers))
+        {
+            throw new \Exception("не введён заголовок From");
+        }
+        $this->headers .= $this->setHeadersFrom();
+        $this->headers .= $this->setHeadersReplyTo();
+        $this->headers .= $this->setHeadersCc();
+        $this->headers .= $this->setHeadersBcc();
+        $this->headers .= $this->setHeadersContentType();
+        if (!empty($this->params["files"]) || !empty($this->files))
+        {
+            $this->headers .= $this->checkFiles($this->files);
+        }
         return $this->headers;
     }
 
-    private function checkFiles($file)
+    private function checkFiles($files)
     {
-        if (!empty($file) && !empty($this->params["file"]))
+        if (!empty($files) && !empty($this->params["files"]))
         {
-           return $fileArrPath = array_merge($file, $this->params["file"]);
-        } elseif (!empty($file))
+            $fileArrPath = array_merge($files, $this->params["files"]);
+        } elseif (!empty($files))
         {
-            return  $file;
-        }   else return $this->params["file"];
-    }
+            $fileArrPath =  $files;
+        }   else $fileArrPath = $this->params["files"];
 
-    private function sendFiles($result)
-    {
-        var_dump($result);
-        foreach ($result as $filePath)
+        foreach ($fileArrPath as $filePath)
         {
             $fileName   = basename($filePath);
             $fileSize   = filesize($filePath);
