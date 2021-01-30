a3b7d795d1c6bb601154f87a6d9e73a3784d23bb
kokocikys <chernecky_ilia@rambler.ru>
2021-01-30 22:07:32 +0300

    class validator V1

--- rii/components/rii/mailer/class.php
+++ rii/components/rii/mailer/class.php
@@ -4,10 +4,13 @@ namespace Rii\Components\Rii;
 
 use Rii\Core\Application;
 use Rii\Core\Component\Base;
+use Rii\Core\Mail\Mail;
 use Rii\Core\Validator\Validator;
 
 class Mailer extends Base
 {
+    private $value = [];
+
     private function hashCheck()
     {
         $requiredHash = $_POST['hash'];
@@ -32,11 +35,10 @@ class Mailer extends Base
 
     private function ourMail()
     {
-        $to = 'elcar24@gmail.com';
-        $subject = 'Заявка на консультацию по телефону';
-        $text = 'Нам поступила заявка на консультацию по телефону от пользователя с именем ' . $_POST['name'] . '!<br>Его номер телефона: ' . $_POST['phone'];
-        $headers = 'Content-type: text/html; charset=utf-8\r\n';
-        mail($to, $subject, $text, $headers);
+        $params['template'] = $this->params['formTemplate'];
+        $params['fields'] = $_POST;
+        $mail = new Mail($params);
+        $mail->send();
         $message = $_POST['name'] . ", cпасибо за обращение! Ожидайте нашего ответа!";
         return $message;
     }
@@ -45,61 +47,76 @@ class Mailer extends Base
     {
         $validationRules = [
             'name' => [
-                (new Validator('required', true))->exec($_POST['name']),
-                (new Validator('minLength', 2))->exec($_POST['name']),
-                (new Validator('maxLength', 20))->exec($_POST['name']),
-            ],
-            'phone' => [
-                (new Validator('required', true))->exec($_POST['phone']),
-                (new Validator('regexp', '/\+?([0-9]{1,3})-?([0-9]{2})-?([0-9]{7})/'))->exec($_POST['phone']),
-            ],
+                (new Validator('required', false, [(new Validator('minLength', 2))->exec($_POST['name']), (new Validator('maxLength', 20))->exec($_POST['name']),]))->exec($_POST['name'])
+//            ],
+//            'phone' => [
+//                new Validator('required', true),
+//                new Validator('phone'),
+//            ],
 //            'password' => [
-//                (new Validator('required', true))->exec($_POST['password']),
-//                (new Validator('minLength', 6))->exec($_POST['password']),
-//                (new Validator('regexp', "/^\S*(?=\S{6,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$/"))->exec($_POST['password']),
+//                new Validator('required', true),
+//                new Validator('minLength', 6),
+//                new Validator('regexp', "/^\S*(?=\S{6,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$/"),
 //            ],
 //            'login' => [
-//                (new Validator('required', true))->exec($_POST['login']),
-//                (new Validator('minLength', 4))->exec($_POST['login']),
-//                (new Validator('maxLength', 20))->exec($_POST['login']),
-//                (new Validator('regexp', '/^[A-Za-z0-9]{0,}$/'))->exec($_POST['login']),
+//                new Validator('required', true),
+//                new Validator('minLength', 4),
+//                new Validator('maxLength', 20),
+//                new Validator('regexp', '/^[A-Za-z0-9]{0,}$/'),
 //            ],
 //            'lastName' => [
-//                (new Validator('required', true))->exec($_POST['lastName'])->exec($_POST['name']),
-//                (new Validator('minLength', 2))->exec($_POST['lastName'])->exec($_POST['name']),
-//                (new Validator('maxLength', 30))->exec($_POST['lastName'])->exec($_POST['name']),
-//                (new Validator('regexp', '/^[A-Za-zА-Яа-яЁё]{0,}$/'))->exec($_POST['lastName'])->exec($_POST['name']),
+//                new Validator('required', true),
+//                new Validator('minLength', 2),
+//                new Validator('maxLength', 30),
+//                new Validator('regexp', '/^[A-Za-zА-Яа-яЁё]{0,}$/'),
 //            ],
 //            'email' => [
-//                (new Validator('required', true))->exec($_POST['email']),
-//                (new Validator('email', true))->exec($_POST['email']),
-//            ]
-        ];
+//                new Validator('required', true),
+//                new Validator('email', true),
+//            ],
+            ]];
+
+//        $validationRules = $this->value;
+
+//        foreach ($validationRules as $key1 => $rule) {
+//            foreach ($rule as $key2 => $item) {
+//                if (!array_key_exists($key1, $_POST)) {
+//                    unset($validationRules[$key1]);
+//                    continue;
+//                }
+//                $validationRules[$key1][$key2] = $item->exec($_POST[$key1]);
+//            }
+//        }
+
+        var_dump($validationRules);
+
         return $validationRules;
     }
 
-    private function error($array)
+    private function errors($array)
     {
-        // Вывод конкретных ошибок
-        foreach ($array as $typeKey=>$item){
-            foreach ($item as $value){
-                var_dump($value);
-                if ($value == false){
-                    $error[] = 'Error';
+        foreach ($array as $typeKey => $item) {
+//            var_dump($typeKey);
+            foreach ($item as $value) {
+//                var_dump($value);
+                if ($value == false) {
+                    $errors[$typeKey] = "{$typeKey} entered incorrectly!";
                 }
             }
         }
-        return $error;
+        return $errors;
     }
 
     public function executeComponent()
     {
         $this->setHashValue($this->params);
         if ($this->hashCheck() == true) {
-            $errors = $this->error($this->validate());
+            if ($_POST['ajax'] != 'yes') die();
+            $errors = $this->errors($this->validate());
+            $this->ourMail();
+            die();
             if ($errors != null) {
-                $error[] = 'Есть ошибки';
-                $this->result['message'] = $error;
+                $this->result['message'] = $errors;
                 Application::getInstance()->restartBuffer();
                 $this->template->render('failed');
                 Application::getInstance()->endBuffer();
--- rii/core/validator/validator.php
+++ rii/core/validator/validator.php
@@ -6,11 +6,13 @@ class Validator
 {
     private $type = '';
     private $set = null;
+    private $valid = null;
 
-    function __construct($function, $param)
+    function __construct($function, $param = null, $valid = null)
     {
         $this->type = $function;
         $this->set = $param;
+        $this->valid = $valid;
     }
 
     function exec($value)
@@ -34,28 +36,37 @@ class Validator
         return filter_var($value, FILTER_VALIDATE_EMAIL);
     }
 
+    function phone($value)
+    {
+        $this->set = '/^\+?([0-9]{1,3})-?([0-9]{2})-?([0-9]{7})$/';
+        return self::regexp($value);
+    }
+
     function required($value)
     {
-        if ($this->set === true) {
-            if ($value == null) {
+        var_dump($this->valid);
+        if ($value == null) {
+            if ($this->set === true) {
                 return false;
-            } else {
-                return true;
+            } else return true;
+        } else {
+            foreach ($this->valid as $rule) {
+                if ($rule == false) {
+                    return false;
+                } else return true;
             }
-        } else return true;
+        }
+//        if ($this->set === true) {
+//            if ($value == null) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+//        } else return true;
     }
 
     function regexp($value)
     {
-        if (!preg_match($this->set, $value)) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function compare($value)
-    {
-        return $value == $this->set;
+        return (bool)preg_match($this->set, $value);
     }
 }
